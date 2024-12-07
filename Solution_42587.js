5a01b79a06e93a285781455089660d91ee30c443
minimin516 <minimin516@gmail.com>
2024-12-07 21:55:15 +0900

    no message

---
+++ Solution_42587.js
@@ -0,0 +1,15 @@
+function solution(priorities, location) {
+    var answer = 0;
+    var queue = priorities.reduce((data, curr, idx) => {
+        data.push({ idx: idx, data: curr });
+        return data;
+    }, []);
+    var t = [];
+    while (queue.length) {
+        const max = queue.reduce((a, b) => (a.data > b.data ? a : b), 0);
+        let first = queue.shift();
+        max.data <= first.data ? t.push(first) : queue.push(first);
+    }
+    answer = t.findIndex((n) => n.idx === location);
+    return answer + 1;
+}
