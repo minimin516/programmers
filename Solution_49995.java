1b3f9f05d438b2e4e08682f792365faf9d3cdf06
minimin516 <minimin516@gmail.com>
2025-01-23 09:29:08 +0900

    no message

---
+++ Solution_49995.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] cookie) {
+        int answer = 0;
+        for (int i = 0; i < cookie.length - 1; i++) {
+            int a = i;
+            int b = i+1;
+            int sumA = cookie[a];
+            int sumB = cookie[b];
+            while (a >= 0 && b < cookie.length) {
+                try {
+                    if (sumA == sumB) {
+                        answer = Math.max(answer, sumA);
+                        sumA += cookie[--a];
+                        sumB += cookie[++b];
+                    } else if (sumA < sumB) {
+                        sumA += cookie[--a];
+                    } else {
+                        sumB += cookie[++b];
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
