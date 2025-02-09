cd361c225351449a12418efd1d1acd2cc21bdaae
minimin516 <minimin516@gmail.com>
2025-02-09 23:34:34 +0900

    no message

--- Solution_42890.java
+++ Solution_42890.java
@@ -14,7 +14,7 @@ public class Solution {
         for(int i = 1; i < (1<<m); i++) {
             Set<String> uniqueStr = new HashSet<>();
             for (int j = 0; j < n; j++) {
-                StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder(); 
                 for (int k = 0; k < m; k++) {
                     if ((i & (1 << k)) > 0)
                         sb.append(relation[j][k]);
