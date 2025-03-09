7ca53dd696f4009357802ca01d4af89ba248fdc7
MINKI <minimin516@gmail.com>
2025-03-09 23:14:29 +0900

    no message

---
+++ Solution_154539.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    const answer = Array.from({ length: numbers.length }, () => -1);
+    const idxArr = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        while (idxArr.length && numbers[idxArr[idxArr.length - 1]] < numbers[i]) {
+            answer[idxArr.pop()] = numbers[i];
+        }
+        idxArr.push(i);
+    }
+    return answer;
+}
